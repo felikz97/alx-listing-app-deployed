b0ab28510e1a6a6315aaebf803a13356012ae503
USER <felikz.kipkemoi@gmail.com>
2025-07-20 22:55:53 +0300

    commit correct

--- components/property/ReviewSection.tsx
+++ components/property/ReviewSection.tsx
@@ -8,7 +8,7 @@ const ReviewSection = ({ propertyId }) => {
     useEffect(() => {
         const fetchReviews = async () => {
         try {
-            const response = await axios.get(`/api/properties/${propertyId}/reviews`);
+            const response = await axios.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/properties/${propertyId}/reviews`);
             setReviews(response.data);
         } catch (error) {
             console.error("Error fetching reviews:", error);
